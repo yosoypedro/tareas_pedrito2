6dc24193d19cc9f485cc3a2391cf95228439f69f
yosoypedro <vpj32011@hotmail.com>
2024-02-17 10:47:51 -0600

    pedrito2
    inicio

---
+++ ejemploprogra5pedrito/src/main/java/ejemplo2/ejemploprogra5pedrito/Ejemploprogra5pedrito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejemplo2.ejemploprogra5pedrito;
+
+/**
+ *
+ * @author HP
+ */
+public class Ejemploprogra5pedrito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
