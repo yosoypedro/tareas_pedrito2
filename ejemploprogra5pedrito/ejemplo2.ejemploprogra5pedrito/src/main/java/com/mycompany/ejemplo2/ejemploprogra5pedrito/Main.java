7053a0fcb9cabe99eaae75c958f399e36c0fc9ae
ElmerMelendrez26 <132164086+catalanwo@users.noreply.github.com>
2024-02-23 22:35:06 -0600

    tarea lista
    Pedrito me esta acarriando

---
+++ ejemploprogra5pedrito/ejemplo2.ejemploprogra5pedrito/src/main/java/com/mycompany/ejemplo2/ejemploprogra5pedrito/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo2.ejemploprogra5pedrito;
+
+/**
+ *
+ * @author Elmer MC3
+ */
+public class Ejemplo2Ejemploprogra5pedrito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
